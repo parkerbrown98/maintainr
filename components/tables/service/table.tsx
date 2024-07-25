2de2e39fa52b16036f11c17642aed5eefce73b4a
Parker Brown <brownpt98@gmail.com>
2024-07-25 01:12:23 -0500

    Add new service record dialog

--- components/tables/service/table.tsx
+++ components/tables/service/table.tsx
@@ -26,6 +26,7 @@ import { DataTableViewOptions } from "@/components/ui/data-table/data-table-view
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { NewServiceRecordDialog } from "@/components/dialogs/new-service-record";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -36,6 +37,7 @@ export function ServiceDataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [serviceRecordDialogOpen, setServiceRecordDialogOpen] = useState(false);
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
@@ -55,81 +57,91 @@ export function ServiceDataTable<TData, TValue>({
   });
 
   return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-between">
-        <Input
-          placeholder="Filter services..."
-          value={
-            (table.getColumn("serviceType")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("serviceType")?.setFilterValue(event.target.value)
-          }
-          className="max-w-xs h-8"
-        />
-        <div className="flex items-center gap-x-2">
-          <DataTableViewOptions table={table} />
-          <Button size="sm" className=" h-8" onClick={() => {}}>
-            <PlusCircle className="mr-2 h-4 w-4" />
-            Add Service
-          </Button>
+    <>
+      <NewServiceRecordDialog
+        open={serviceRecordDialogOpen}
+        setOpen={setServiceRecordDialogOpen}
+      />
+      <div className="space-y-4">
+        <div className="flex items-center justify-between">
+          <Input
+            placeholder="Filter services..."
+            value={
+              (table.getColumn("serviceType")?.getFilterValue() as string) ?? ""
+            }
+            onChange={(event) =>
+              table.getColumn("serviceType")?.setFilterValue(event.target.value)
+            }
+            className="max-w-xs h-8"
+          />
+          <div className="flex items-center gap-x-2">
+            <DataTableViewOptions table={table} />
+            <Button
+              size="sm"
+              className=" h-8"
+              onClick={() => setServiceRecordDialogOpen(true)}
+            >
+              <PlusCircle className="mr-2 h-4 w-4" />
+              Add Service
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {cell.getValue() ? (
-                        flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )
-                      ) : (
-                        <span>N/A</span>
-                      )}
-                    </TableCell>
-                  ))}
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {cell.getValue() ? (
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )
+                        ) : (
+                          <span>N/A</span>
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <DataTablePagination table={table} />
       </div>
-      <DataTablePagination table={table} />
-    </div>
+    </>
   );
 }

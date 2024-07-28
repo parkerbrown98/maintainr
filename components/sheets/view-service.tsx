0e1a3f0e7ff6a1010bad25e13b0050ed63991d03
Parker Brown <brownpt98@gmail.com>
2024-07-27 23:09:11 -0500

    Add some service details to service detail sheet

--- components/sheets/view-service.tsx
+++ components/sheets/view-service.tsx
@@ -13,24 +13,96 @@ import { SERVICE_TYPES } from "@/lib/constants";
 import { Button } from "../ui/button";
 import { useSheet } from "@/lib/hooks/sheet";
 import { useService } from "@/lib/hooks/service";
+import { Separator } from "../ui/separator";
+import moment from "moment";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { useVehicle } from "@/lib/hooks/vehicle";
 
 export function ViewServiceSheet() {
   const { open, setOpen } = useSheet();
+  const vehicle = useVehicle();
   const { service } = useService();
 
-  if (!service) {
+  if (!service || !vehicle) {
     return null;
   } else {
     const serviceType = service.serviceType as keyof typeof SERVICE_TYPES;
+    const serviceDate = moment(service.serviceDate).format("MMMM Do, YYYY");
 
     return (
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>{SERVICE_TYPES[serviceType]}</SheetTitle>
-            <SheetDescription>{service.description}</SheetDescription>
+            <SheetTitle>Service Details</SheetTitle>
+            <SheetDescription>View details about this service</SheetDescription>
           </SheetHeader>
+          <div className="py-6 flex flex-col space-y-6">
+            <div className="p-4 flex items-center justify-between bg-muted border border-border rounded-md">
+              <div className="flex items-center gap-x-2">
+                <Avatar>
+                  <AvatarFallback className="bg-gray-200">
+                    {vehicle.make[0].toUpperCase()}
+                    {vehicle.model[0].toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <div className="text-sm font-medium leading-tight">{vehicle.year} {vehicle.make}</div>
+                  <div className="text-sm text-muted-foreground">{vehicle.model}</div>
+                </div>
+              </div>
+              <div>
+                <Button variant="outline" size="sm">View Vehicle</Button>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <div className="font-medium">Information</div>
+              <Separator />
+              <div className="flex justify-between items-center text-sm">
+                <div className="text-muted-foreground font-medium">
+                  Service Type
+                </div>
+                <div>{SERVICE_TYPES[serviceType]}</div>
+              </div>
+              <Separator />
+              <div className="flex justify-between items-center text-sm">
+                <div className="text-muted-foreground font-medium">Cost</div>
+                <div>
+                  {Number(service.cost).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </div>
+              </div>
+              <Separator />
+              <div className="flex justify-between items-center text-sm">
+                <div className="text-muted-foreground font-medium">Date</div>
+                <div>{serviceDate}</div>
+              </div>
+              <Separator />
+              <div className="flex justify-between items-center text-sm">
+                <div className="text-muted-foreground font-medium">
+                  Odometer
+                </div>
+                <div>{service.odometer ? service.odometer + " mi" : "N/A"}</div>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <div className="font-medium">Notes</div>
+              <div className="text-sm text-muted-foreground">
+                {service.description ?? "No description provided."}
+              </div>
+            </div>
+            <div className="space-y-2">
+              <div className="font-medium">Uploads</div>
+              <div className="text-sm text-muted-foreground">
+                No uploads have been attached to this service.
+              </div>
+            </div>
+          </div>
           <SheetFooter>
+            <Button variant="destructive" onClick={() => setOpen(false)}>
+              Delete
+            </Button>
             <Button variant="outline" onClick={() => setOpen(false)}>
               Close
             </Button>

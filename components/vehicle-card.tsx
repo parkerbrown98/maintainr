37e9924e0a20d954c3c842037b6eadbc0567dac3
Parker Brown <brownpt98@gmail.com>
2024-07-28 12:34:01 -0500

    Add correct odometer reading to dashboard vehicle cards

--- components/vehicle-card.tsx
+++ components/vehicle-card.tsx
@@ -1,4 +1,4 @@
-import { Vehicle } from "@/drizzle/schema";
+import { odometerReadings, Vehicle } from "@/drizzle/schema";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -9,26 +9,43 @@ import {
   CardTitle,
 } from "./ui/card";
 import Link from "next/link";
+import { coalesce, db } from "@/lib/db";
+import { eq, max, sql } from "drizzle-orm";
+import { UnitFormat } from "./unit-format";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 
 interface VehicleCardProps {
   vehicle: Vehicle;
 }
 
-export default function VehicleCard({ vehicle }: VehicleCardProps) {
+export default async function VehicleCard({ vehicle }: VehicleCardProps) {
+  const [odometerReading] = await db
+    .select({ reading: coalesce(max(odometerReadings.reading), sql`0`) })
+    .from(odometerReadings)
+    .where(eq(odometerReadings.vehicleId, vehicle.id));
+
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>
-          {vehicle.year} {vehicle.make} {vehicle.model}
-        </CardTitle>
-        <CardDescription>{vehicle.vin ?? "No VIN provided"}</CardDescription>
+      <CardHeader className="flex flex-row items-center gap-x-4">
+        <Avatar className="w-12 h-12">
+          <AvatarFallback>
+            {vehicle.make.charAt(0).toUpperCase()}
+            {vehicle.model.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+        <div>
+          <CardTitle>
+            {vehicle.year} {vehicle.make} {vehicle.model}
+          </CardTitle>
+          <CardDescription>bing bong</CardDescription>
+        </div>
       </CardHeader>
       <CardContent>
         <div className="flex items-center justify-between flex-wrap gap-4">
           <div>
             <p className="text-sm text-muted-foreground">Odometer</p>
             <p className="text-lg font-bold">
-              96,360 mi
+              <UnitFormat value={odometerReading.reading ?? 0} unit="length" />
             </p>
           </div>
           <div>

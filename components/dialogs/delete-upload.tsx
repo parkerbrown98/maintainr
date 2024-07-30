4879233ab2241d283295b989c62c9a748ccb746f
Parker Brown <brownpt98@gmail.com>
2024-07-29 23:42:12 -0500

    Add delete upload dialog component

---
+++ components/dialogs/delete-upload.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Upload } from "@/drizzle/schema";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import { deleteUpload } from "@/lib/actions/uploads";
+
+interface DeleteUploadDialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  upload: Upload;
+}
+
+export function DeleteUploadDialog({
+  open,
+  setOpen,
+  upload,
+}: DeleteUploadDialogProps) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      setLoading(true);
+
+      const result = await deleteUpload(upload.id);
+      if (result && result.error) {
+        throw new Error(result.error);
+      }
+
+      setOpen(false);
+      router.refresh();
+    } catch (err) {
+      if (err instanceof Error) {
+        toast.error(err.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete upload</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this upload? It will be permanently
+            destroyed.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={handleDelete} disabled={loading}>
+            {loading ? "Working" : "Delete"}
+          </Button>
+          <Button variant="outline" onClick={() => setOpen(false)}>
+            Cancel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}

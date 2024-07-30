332f444995060ca020e21bcabd9bdf23d8bdfbcc
Parker Brown <brownpt98@gmail.com>
2024-07-29 23:41:02 -0500

    Finish uploads and saving file to disk logic, better validation

--- lib/actions/uploads.ts
+++ lib/actions/uploads.ts
@@ -6,10 +6,10 @@ import fs from "fs";
 import { writeFile } from "fs/promises";
 import path from "path";
 import { validateUser } from "../auth";
+import { and, eq } from "drizzle-orm";
 
-export async function saveFileToDisk(file: File) {
+export async function saveFileToDisk(fileName: string, file: File) {
   const saveDir = path.join(process.cwd(), "uploads");
-  const fileName = file.name.replaceAll(" ", "_");
   const filePath = path.join(saveDir, fileName);
 
   if (!fs.existsSync(saveDir)) {
@@ -19,7 +19,7 @@ export async function saveFileToDisk(file: File) {
   const buffer = Buffer.from(await file.arrayBuffer());
   await writeFile(filePath, buffer);
 
-  return filePath;
+  return path.join("/uploads", fileName);
 }
 
 export async function createUpload(form: FormData) {
@@ -37,10 +37,16 @@ export async function createUpload(form: FormData) {
     return { error: "No file found" };
   }
 
-  const filePath = await saveFileToDisk(file);
+  let fileName = file.name.replaceAll(" ", "_");
+
+  // Insert a random string to avoid overwriting files with the same name
+  const randomString = Math.random().toString(36).substring(7);
+  fileName = `${randomString}_${fileName}`;
+
+  const filePath = await saveFileToDisk(fileName, file);
 
   await db.insert(uploads).values({
-    fileName: file.name,
+    fileName,
     mimeType: file.type,
     userId: userObj.user.id,
     size: file.size,
@@ -48,4 +54,31 @@ export async function createUpload(form: FormData) {
   });
 }
 
-export async function deleteUpload(id: string) {}
+export async function deleteUpload(id: string) {
+  const userObj = await validateUser();
+  if (!userObj) {
+    return { error: "User not found" };
+  }
+
+  const [upload] = await db
+    .select()
+    .from(uploads)
+    .where(and(eq(uploads.id, id), eq(uploads.userId, userObj.user.id)))
+    .limit(1);
+
+  if (!upload) {
+    return { error: "Upload not found" };
+  }
+
+  try {
+    fs.unlinkSync(path.join(process.cwd(), upload.url));
+  } catch (err) {
+    return { error: "Failed to delete file from filesystem" };
+  }
+
+  const result = await db
+    .delete(uploads)
+    .where(and(eq(uploads.id, id), eq(uploads.userId, userObj.user.id)));
+
+  return null;
+}
